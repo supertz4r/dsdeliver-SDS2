8dfcf1c7e7b9c5f4771d423c2a3e3dde347d607d
Júnior Oliveira <juninsoader@gmail.com>
2021-01-07 12:18:36 -0300

    routes e StepsHeader adicionados

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css';
-import Home from './Home';
 import Navbar from './NavBar';
+import Routes from './Routes';
 
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <Routes />
   );
 }
 
