e4aaa916d836249dd50c0da6833853c90010fb63
Júnior Oliveira <juninsoader@gmail.com>
2021-01-07 11:27:44 -0300

    Home adicionada

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
